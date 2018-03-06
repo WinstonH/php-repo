48c9fb752570f768bc3f4188bf0e8c9a27d8500e
WinstonH <bjdhjj@gmail.com>
2018-03-06 17:47:02 +0800

    Update getip.php

--- getip.php
+++ getip.php
@@ -1,28 +1,19 @@
 <?php
-  // Function to get the client IP address
-  function get_client_ip() {
-    $ipaddress = '';
-    if (getenv('HTTP_CLIENT_IP'))
-        $ipaddress = getenv('HTTP_CLIENT_IP');
-    else if(getenv('HTTP_X_FORWARDED_FOR'))
-        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-    else if(getenv('HTTP_X_FORWARDED'))
-       	$ipaddress = getenv('HTTP_X_FORWARDED');
-    else if(getenv('HTTP_FORWARDED_FOR'))
-       	$ipaddress = getenv('HTTP_FORWARDED_FOR');
-    else if(getenv('HTTP_FORWARDED'))
-       $ipaddress = getenv('HTTP_FORWARDED');
-    else if(getenv('REMOTE_ADDR'))
-       $ipaddress = getenv('REMOTE_ADDR');
-    else
-       $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-  }
-  
-  $ip = get_client_ip();	
-  $posicao = strpos($ip, ', ');
-  if (substr($ip, $posicao, 2) == ', ')
-     echo substr($ip, $posicao+2);
-  else
-     echo substr($ip, $posicao);
-?>
+function GetIP(){
+	if(getenv("HTTP_CLIENT_IP")) {
+ 		$ip = getenv("HTTP_CLIENT_IP");
+ 	} elseif(getenv("HTTP_X_FORWARDED_FOR")) {
+ 		$ip = getenv("HTTP_X_FORWARDED_FOR");
+ 		if (strstr($ip, ',')) {
+ 			$tmp = explode (',', $ip);
+ 			$ip = trim($tmp[0]);
+ 		}
+ 	} elseif(getenv("REMOTE_ADDR")) {
+	$ip = getenv("REMOTE_ADDR");	
+	} else {
+ 	$ip = 'localhost';
+ 	}
+	return $ip;
+}
+
+echo $_SERVER['REMOTE_ADDR'];
